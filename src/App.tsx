d2fe22c3c8a918c30528e8c2b944f454c66f510e
7i7o <mparij@communitylabs.com>
2024-09-01 18:01:14 +0200

    update favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { ConnectButton, useConnection } from 'arweave-wallet-kit';
 import { ArMessenger } from './components/ArMessenger';
+import logo from './assets/logo.svg';
 
 export const AO_ID = '7MCaTzo1teKUywMvFU_gnr-hAeEmkaM4xMvRRerWUdo';
 
@@ -10,7 +11,7 @@ function App() {
     return (
         <>
             <ConnectButton className="connect-button" showBalance={false} />
-
+            <img src={logo} alt="Logo" width="100" />
             <h1>Arweave Messenger</h1>
             {!connected && (
                 <>
